5108c631e273421aea6870af665723d5f7628831
otmanesajid <otmanesajid@outlook.fr>
2023-09-18 15:15:35 +0100

    result

--- kaggle titanic.py
+++ kaggle titanic.py
@@ -2,20 +2,44 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
 
 # Load the data
-data_1 = pd.read_csv("train.csv")
-data_2 = pd.read_csv("test.csv")
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
 
-# Separate features (X) and target variable (y)
-X_train = data_1.dropna().drop_duplicates()  # Assuming you want to drop rows with missing values
-X_test = data_2.dropna().drop_duplicates()
-y_train = X_train['Transported']  # Replace 'target_column' with the actual name of your target variable
+train=train_data.drop(['PassengerId','Cabin','Name'],axis=1)
+test=test_data.drop(['PassengerId','Cabin','Name'],axis=1)
 
-features = ['PassengerId', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck', 'CryoSleep', 'VIP']
+train.isnull().sum()
+test.isnull().sum()
+
+
+
+
+train.fillna(train[['Age','RoomService','FoodCourt','ShoppingMall','Spa','VRDeck']].apply(lambda n:n.mean()),inplace=True)
+test.fillna(test[['Age','RoomService','FoodCourt','ShoppingMall','Spa','VRDeck']].apply(lambda n:n.mean()),inplace=True)
+
+
+
+cols=['HomePlanet','CryoSleep','Destination','VIP','Transported']
+Cols=['HomePlanet','CryoSleep','Destination','VIP']
+
+le=LabelEncoder()
+for col in cols:
+    train[col]=le.fit_transform(train[col])
+for col in Cols:
+    test[col]=le.transform(test[col])
+
+
+train = train.ffill()
+test = test.bfill()
+
+X=train.drop('Transported',axis=1)
+y=train['Transported']
 
 # Create a validation set
-X_train, X_val, y_train, y_val = train_test_split(X_train[features], y_train, test_size=0.2, random_state=1)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=1)
 
 # Train the model
 model = LogisticRegression()
@@ -33,13 +57,17 @@ class_report = classification_report(y_val, predictions_val)
 print("Classification Report on validation data:\n", class_report)
 
 # Predict on the test data
-predictions_test = model.predict(X_test[features])
+predictions_test = model.predict(test)
 
-# Now, create a DataFrame with PassengerId and predictions
-passenger_predictions = pd.DataFrame({'PassengerId': X_test['PassengerId'], 'Predicted_Transported': predictions_test})
+
+
+predictions_test = [True if pred == 1 else False for pred in predictions_test]
+
+passenger_predictions = pd.DataFrame({'PassengerId': test_data['PassengerId'], 'Transported': predictions_test})
+
 
 # Now, predictions_test contains the predicted values for the test data
-print("Predictions for test data:", predictions_test)
+print("Predictions for test data:", passenger_predictions)
 
 # Save passenger_predictions to a CSV file
-passenger_predictions.to_csv('submition.csv', index=False)
+passenger_predictions.to_csv('C:\\Users\\pc\\Desktop\\submission.csv', index=False)
